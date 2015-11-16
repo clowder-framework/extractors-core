c2969fd2d6b2cf23affcdb3f7e8f14590a650207
Max Burnette <mburnet2@illinois.edu>
2015-11-16 16:03:57 -0600

    Updated to preserve nested EXIF & data types
    Now nested objects in EXIF data will be nested in resulting JSON
    metadata as well. Two structures - Histogram and Colormap - currently
    use special handling. Also, strings undergo simple RegEx to identify
    ints, floats and boolean values.

--- metadata/ncsa.image.preview.py
+++ metadata/ncsa.image.preview.py
@@ -18,33 +18,33 @@ def main():
 
 # ----------------------------------------------------------------------
 def parseExif(text):
-    # adapted from https://github.com/dandean/imagemagick-identify-parser
+    # Adapted from https://github.com/dandean/imagemagick-identify-parser
     text = str(text)
-
     if (text.strip() == ''):
         return {}
 
     # Each new line should start with *at least* two spaces. This fixes 1st line.
     lines = ('  ' + text.strip()).split("\n")
 
+    # Our 'stack' will be a list of objects. The first object will be returned;
+    # others are temporary objects to be nested in the main object.
+    data = [{}]
     lastDepth=1
     lastKey=""
-    data={"raw": text}
 
-    # parse all lines
     for line in lines:
-        # skip empty lines
-        if (line.strip() == ''):
+        if line.strip() == '':
             continue
 
+        # Clear out encoded line breaks
         line = line.replace("\\r", "").replace("\\n", "").replace("\r", "").replace("\n", "")
 
         # The line *must* contain a colon to be processed.
         try:
             index=line.index(':')
             if index > -1:
-                next_char = line[index+1] if len(line) > index+1 else None
                 # next_char is undefined when ':' is last char on the line
+                next_char = line[index+1] if len(line) > index+1 else None
                 if next_char and re.match("/\w/", next_char):
                     # start counting from the first ':'
                     for j in range(index+1, line.length):
@@ -52,54 +52,92 @@ def parseExif(text):
                             index = j
                             break
 
-            depth=len(re.match(" +", line).group(0)) / 2
-            key = line[0:index].strip()
+            depth = int(len(re.match(" +", line).group(0)) / 2)
+            key   = line[0:index].strip()
             value = line[index+1:].strip() if len(line) > index+1 else {}
-            #print("%d %d" % (index, depth))
 
-            if depth==1 and re.match("/^Geometry$/i", key) and re.match("/^\d+x\d+\+\d+\+\d+$/", value):
-                # Extract width and height from geometry property if present and value
-                # is in format "INTxINT+INT+INT"
-                parts = value.split('x')
-                data["width"] = parts[0]
-                data["height"] = parts[1]
+            # Attempt to interpret data types from strings
+            if type(value) is str:
+                if re.match("^\-?\d+$", value):
+                    value = int(value)
+                elif re.match("^\-?\d+?\.\d+$", value):
+                    value = float(value)
+                elif re.match("^true$", value, flags=re.IGNORECASE):
+                    value = True
+                elif re.match("^false$", value, flags=re.IGNORECASE):
+                    value = False
+                elif re.match("^undefined$", value, flags=re.IGNORECASE):
+                    continue
 
-            if depth==lastDepth:
-                # Add the key/value pair to the last object in the stack
-                data[key] = value
+            if depth==1 and re.match(".*Geometry$", key, flags=re.IGNORECASE) and re.match("^\d+x\d+\+\d+\+\d+$", value):
+                # Extract width and height from "_Geometry" (e.g "Pagegeometry") property if present.
+                # value must be in format "INTxINT+INT+INT".
+                # Full raw entry will still be added to final result.
+                parts = value.split('+')[0].split('x')
+                data[0]["width"] = int(parts[0])
+                data[0]["height"] = int(parts[1])
 
-                # Note this key as the last key, which will become the parent key if the next object is a child.
+            if (lastKey == "Histogram" or lastKey == "Colormap") and depth>1:
+                # These two must be handled separately because ImageMagick does not align first digit of their lines
+                # so the depths get confused, e.g.
+                #        0: (  0,  0,  0) #000000 gray(0)
+                #       10: ( 10, 10, 10) #0A0A0A gray(10)
+                #      100: (100,100,100) #646464 gray(100)
+                # These are really all "depth+1" whatever depth the original Histogram/colormap was
+                if lastKey in data[len(data)-1]:
+                    # First entry for this histogram/colormap, so make it a separate object in data list for now
+                    sub_data = data[len(data)-1].pop(lastKey)
+                    sub_data[key] = value
+                    sub_data["_EXIF_PARENT_KEY_NAME"] = lastKey # This lets us nest recursively if needd
+                    data.append(sub_data)
+                    lastDepth += 1
+                else:
+                    if lastKey == "Histogram":
+                        # Flip key/value because key is pixel count - not unique
+                        data[len(data)-1][value] = int(key)
+                    else:
+                        data[len(data)-1][key] = value # Colormap
+
+            elif depth==lastDepth:
+                # Add the key/value pair to the last object in the stack.
+                # Key will become the parent key if the next value is a child.
+                data[len(data)-1][key] = value
                 lastKey = key
 
             elif depth==lastDepth+1:
-                # Add the last key (which should be an empty object) to the end of
-                # the object stack. This allows us to match stack depth to
-                # indentation depth.
-                #stack.push(stack[stack.length-1][lastKey])
-                data[key] = value
-
-                lastDepth+=1
+                # Pop last key (which should be an empty object) from the main object and append
+                # to the end of the list, adding key/value pair to it. Depth is maintained.
+                sub_data = data[len(data)-1].pop(lastKey)
+                sub_data[key] = value
+                sub_data["_EXIF_PARENT_KEY_NAME"] = lastKey # This lets us nest recursively if needd
+                data.append(sub_data)
+                lastDepth += 1
                 lastKey = key
 
             elif depth < lastDepth:
-                # Remove items from the end of the stack so that we add this new
+                # Remove object(s) from end of the stack so that we add this new
                 # key/value pair to the correct parent object.
-                #stack = stack.slice(0, depth);
-                data[key] = value;
-
-                lastDepth = depth;
-                lastKey = key;
+                while lastDepth != depth:
+                    lastDepth -= 1
+                    sub_data = data.pop()
+                    parent_key = sub_data.pop("_EXIF_PARENT_KEY_NAME")
+                    data = data[:lastDepth]
+                    data[len(data)-1][parent_key] = sub_data
+                data[len(data)-1][key] = value
+                lastKey = key
 
         except Exception as e:
             print(e)
             print("Skipping : " + line)
 
-    return data
+    # Add raw source onto the primary dictionary object and return it
+    data[0]["raw"] = text
+    return data[0]
 
 # Process the file and upload the results
 def process_file(parameters):
     global imageBinary
-    print(parameters)
+
     input_file = parameters['inputfile']
     clowder_host = parameters['host']
     result = parseExif(subprocess.check_output([imageBinary, "-verbose", input_file], stderr=subprocess.STDOUT))
